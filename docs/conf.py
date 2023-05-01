2a1f097ca9bee636f0d7b03939a3235f8818e00e
DaniDaniel09 <114660597+DaniDaniel09@users.noreply.github.com>
2023-05-01 22:48:11 +0200

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'documentacionEDD'
+copyright = '2023, Daniel Zanon Lopez/DaniDaniel09'
+author = 'Daniel Zanon Lopez/DaniDaniel09'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# OS path commands should be uncommented in conf.py file
+# so that html utility could access the right project files to 
+# generate documentation. 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. documentacionEDD documentation master file, created by
+   sphinx-quickstart on Fri Apr 28 13:22:36 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to documentacionEDD's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
